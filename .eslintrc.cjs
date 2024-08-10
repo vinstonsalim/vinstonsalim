d529454e492fff8acdf69129dfa49e7ab5e96489
Vinston Salim <vinston.salim@outlook.com>
2024-08-10 21:17:28 +0200

    Add eslint and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,48 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/strict",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./vite.config.ts"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  ignorePatterns: [
+    "node_modules",
+    "dist",
+    ".eslintrc.cjs",
+    "vite-env.d.ts",
+    "vite.config.ts",
+  ],
+  rules: {
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "spaced-comment": [
+      "error",
+      "always",
+      { exceptions: ["-", "+"], markers: ["!", "TODO", "?", "//"] },
+    ],
+    "capitalized-comments": [
+      "error",
+      "always",
+      {
+        ignorePattern: "pragma|ignored",
+        ignoreInlineComments: true,
+      },
+    ],
+    curly: ["error", "multi"],
+  },
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Could not find root element");
+
+ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
